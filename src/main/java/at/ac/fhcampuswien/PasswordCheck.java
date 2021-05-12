20e690d2a79e3839a4b70dd485ad540af21377e2
Joseph <71464700+jhangstein@users.noreply.github.com>
2021-05-12 20:51:54 +0200

    PW length Tests positive.

--- src/main/java/at/ac/fhcampuswien/PasswordCheck.java
+++ src/main/java/at/ac/fhcampuswien/PasswordCheck.java
@@ -1,7 +1,10 @@
 package at.ac.fhcampuswien;
 
 public class PasswordCheck {
-    public boolean isValid(String pw){
-        return false;
+    public boolean isValid(String pw) {
+        if (pw.length()<= 7 || pw.length() >= 24) {
+            return false;
+        }
+        return true;
     }
 }
