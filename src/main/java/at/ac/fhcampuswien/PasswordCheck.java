b18669734b480a5241cf12db420fb3f8f506e51d
Joseph <71464700+jhangstein@users.noreply.github.com>
2021-05-12 20:45:01 +0200

    Implemented Test Cases for PW length.

--- src/main/java/at/ac/fhcampuswien/PasswordCheck.java
+++ src/main/java/at/ac/fhcampuswien/PasswordCheck.java
@@ -1,4 +1,7 @@
 package at.ac.fhcampuswien;
 
 public class PasswordCheck {
+    public boolean isValid(String pw){
+        return false;
+    }
 }
--- src/test/java/PasswordCheckTest.java
+++ src/test/java/PasswordCheckTest.java
@@ -1,2 +1,28 @@
+import at.ac.fhcampuswien.PasswordCheck;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PasswordCheckTest {
+
+    @Test
+    @DisplayName("Check valid pw length")
+    public void testPasswordLength(){
+        PasswordCheck pw = new PasswordCheck();
+        assertTrue(pw.isValid("password"));
+    }
+
+    @Test
+    @DisplayName("Check invalid pw length")
+    public void testPasswordLength2(){
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.isValid("pass"));
+    }
+
+    @Test
+    @DisplayName("Check invalid pw length")
+    public void testPasswordLength3(){
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.isValid("supermegaultrasecurepassword"));
+    }
 }
