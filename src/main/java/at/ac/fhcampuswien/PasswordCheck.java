b6c06c4598eabe47ca85cde5d232d037f96b912b
Joseph <71464700+jhangstein@users.noreply.github.com>
2021-05-13 13:26:30 +0200

    Refactored PasswordCheck Class and added upper/lower Tests + implementation

--- src/main/java/at/ac/fhcampuswien/PasswordCheck.java
+++ src/main/java/at/ac/fhcampuswien/PasswordCheck.java
@@ -1,10 +1,30 @@
 package at.ac.fhcampuswien;
 
 public class PasswordCheck {
-    public boolean isValid(String pw) {
-        if (pw.length()<= 7 || pw.length() >= 24) {
-            return false;
+    public boolean checkPassword(String pw) {
+        if (pwLength(pw)){
+            if (pwContainsUpperLowerCase(pw)){
+                return true;
+            }
         }
-        return true;
+        return false;
+    }
+
+    public boolean pwLength(String pw){
+        return pw.length() > 7 && pw.length() < 24;
+    }
+
+    public boolean pwContainsUpperLowerCase(String pw){
+        boolean containsLower = false, containsUpper = false;
+        for(int i = 0; i < pw.length(); i++){
+            if (pw.charAt(i) >= 65 && pw.charAt(i) <= 90){
+                containsUpper = true;
+            }
+            else if (pw.charAt(i) >= 97 && pw.charAt(i) <= 122){
+                containsLower = true;
+            }
+        }
+        if (containsLower && containsUpper) return true;
+        else return false;
     }
 }
--- src/test/java/PasswordCheckTest.java
+++ src/test/java/PasswordCheckTest.java
@@ -5,24 +5,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordCheckTest {
 
+    // Testing password length
     @Test
     @DisplayName("Check valid pw length")
     public void testPasswordLength(){
         PasswordCheck pw = new PasswordCheck();
-        assertTrue(pw.isValid("password"));
+        assertTrue(pw.pwLength("Password"));
     }
 
     @Test
     @DisplayName("Check invalid pw length")
     public void testPasswordLength2(){
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.isValid("pass"));
+        assertFalse(pw.pwLength("pass"));
     }
 
     @Test
     @DisplayName("Check invalid pw length")
     public void testPasswordLength3(){
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.isValid("supermegaultrasecurepassword"));
+        assertFalse(pw.pwLength("supermegaultrasecurepassword"));
     }
+
+
+    // Testing Upper/Lower Case requirements
+    @Test
+    @DisplayName("Check valid Aa")
+    public void testPasswordUpperLower(){
+        PasswordCheck pw = new PasswordCheck();
+        assertTrue(pw.pwContainsUpperLowerCase("VerySecure"));
+    }
+
+    @Test
+    @DisplayName("Check invalid Aa")
+    public void testPasswordUpperLower2(){
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.pwContainsUpperLowerCase("verysecure"));
+    }
+
+    @Test
+    @DisplayName("Check invalid Aa")
+    public void testPasswordUpperLower3(){
+        PasswordCheck pw = new PasswordCheck();
+        assertFalse(pw.pwContainsUpperLowerCase("123456789"));
+    }
+
+
+
 }
