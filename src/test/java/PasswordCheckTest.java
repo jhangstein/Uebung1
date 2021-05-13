5deecf3182eba226171e0b911f3def1a20cf5432
Joseph <71464700+jhangstein@users.noreply.github.com>
2021-05-13 13:27:57 +0200

    Slightly adjusted Tests

--- src/test/java/PasswordCheckTest.java
+++ src/test/java/PasswordCheckTest.java
@@ -10,21 +10,21 @@ public class PasswordCheckTest {
     @DisplayName("Check valid pw length")
     public void testPasswordLength(){
         PasswordCheck pw = new PasswordCheck();
-        assertTrue(pw.pwLength("Password"));
+        assertTrue(pw.checkPassword("Password"));
     }
 
     @Test
     @DisplayName("Check invalid pw length")
     public void testPasswordLength2(){
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.pwLength("pass"));
+        assertFalse(pw.checkPassword("pass"));
     }
 
     @Test
     @DisplayName("Check invalid pw length")
     public void testPasswordLength3(){
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.pwLength("supermegaultrasecurepassword"));
+        assertFalse(pw.checkPassword("supermegaultrasecurepassword"));
     }
 
 
@@ -33,21 +33,21 @@ public class PasswordCheckTest {
     @DisplayName("Check valid Aa")
     public void testPasswordUpperLower(){
         PasswordCheck pw = new PasswordCheck();
-        assertTrue(pw.pwContainsUpperLowerCase("VerySecure"));
+        assertTrue(pw.checkPassword("VerySecure"));
     }
 
     @Test
     @DisplayName("Check invalid Aa")
     public void testPasswordUpperLower2(){
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.pwContainsUpperLowerCase("verysecure"));
+        assertFalse(pw.checkPassword("verysecure"));
     }
 
     @Test
     @DisplayName("Check invalid Aa")
     public void testPasswordUpperLower3(){
         PasswordCheck pw = new PasswordCheck();
-        assertFalse(pw.pwContainsUpperLowerCase("123456789"));
+        assertFalse(pw.checkPassword("123456789"));
     }
 
 
